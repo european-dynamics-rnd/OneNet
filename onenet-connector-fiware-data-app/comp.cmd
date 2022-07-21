d531d3a40ffe120461c668d30b614870533df69d
Ferdinando Bosco <ferdinando.bosco@eng.it>
2022-07-21 11:39:00 +0200

    First Relase one-step ready

--- onenet-connector-fiware-data-app/comp.cmd
+++
@@ -1,8 +0,0 @@
-echo on
-
-REM # BUILD
-@call mvn clean install -U -e
-
-REM # MAKE docker image
-REM # sudo docker build -t data-app-eng:0.1 .
-@call docker build -t onenet-connector-fiware-data-app:0.1 .
